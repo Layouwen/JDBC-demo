de4c141bd34c34b6d5e657dc529e828e0ac4aadc
liangyouwen <layouwen@gmail.com>
2020-11-28 14:30:05 +0800

    完成login2，防止SQL注入

--- src/cn/lyw/jdbc/JDBCDemo09.java
+++ src/cn/lyw/jdbc/JDBCDemo09.java
@@ -2,10 +2,7 @@ package cn.lyw.jdbc;
 
 import cn.lyw.util.JDBCUtils;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Scanner;
 
 /**
@@ -20,7 +17,7 @@ public class JDBCDemo09 {
     String name = sc.nextLine();
     System.out.println("请输入密码:");
     String password = sc.nextLine();
-    boolean flag = new JDBCDemo09().login(name, password);
+    boolean flag = new JDBCDemo09().login2(name, password);
     if (flag) {
       System.out.println("登录成功");
     } else {
@@ -48,6 +45,32 @@ public class JDBCDemo09 {
       e.printStackTrace();
     } finally {
       JDBCUtils.close(rs, stmt, conn);
+    }
+    return false;
+  }
+
+  public boolean login2(String name, String password) {
+    if (name == null || password == null) return false;
+    Connection conn = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+    try {
+      // 获取连接
+      conn = JDBCUtils.getConnection();
+      // 定义sql
+      String sql = "SELECT * FROM user WHERE username = ? and password = ?";
+      // 获取执行的sql对象
+      pstmt = conn.prepareStatement(sql);
+      pstmt.setString(1, name);
+      pstmt.setString(2, password);
+      // 执行sql语句
+      rs = pstmt.executeQuery();
+      // 判断是否存在
+      return rs.next();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      JDBCUtils.close(rs, pstmt, conn);
     }
     return false;
   }
